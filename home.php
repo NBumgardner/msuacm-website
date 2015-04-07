d6dbd6a35189a712686c6906738aa594367c5c59
unknown <ntb128@CHEK-0210-14.SGF.EDUBEAR.NET>
2015-04-07 18:34:54 -0500

    Added Meetings h1. Added '.' to paragraphs.

--- home.php
+++ home.php
@@ -7,16 +7,20 @@
 
 <body>
 
+<h1>Meetings</h1>
+<p>Next meeting on 2/24/2015 at 6 P. M. in Cheek 210.</p>
+
 <h1 id="collaborators">Collaborators</h1>
-<p>Matt is a collaborator</p>
-<p>Matt P. is a collaborator</p>
+<p>Matt is a collaborator.</p>
+<p>Matt P. is a collaborator.</p>
 <p>Mitch is a swag nerd baller who is using vim to type.</p>
-<p>Cameron Yuan is a collaborator</p>
-<p>Garren is a collaborator</p>
-<p>Greg is a collaborator</p>
-<p>Zach is a collaborator</p>
-<p>Clint is a collaborator</p>
-<p>Topher is a collaborator</p>
+<p>Cameron Yuan is a collaborator.</p>
+<p>Garren is a collaborator.</p>
+<p>Greg is a collaborator.</p>
+<p>Zach is a collaborator.</p>
+<p>Clint is a collaborator.</p>
+<p>Topher is a collaborator.</p>
+<p>Noah is a collaborator.</p>
 
 </body>
 </html>
